bcde7ae2b071c0c80e9ac7a31b99fff657f2dd87
Salvador Juan Martinez <saljuama@gmail.com>
2020-07-05 18:25:45 +0200

    configure databases for run in local and tests

--- src/test/kotlin/dev/saljuama/demo/ksj/ApplicationTests.kt
+++ src/test/kotlin/dev/saljuama/demo/ksj/ApplicationTests.kt
@@ -2,7 +2,9 @@ package dev.saljuama.demo.ksj
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
+@ActiveProfiles("integration-test")
 @SpringBootTest
 class ApplicationTests {
 
