baaa97b42745b69d5eac8fd0b6d367e783327553
Salvador Juan Martinez <saljuama@gmail.com>
2020-08-04 18:19:49 +0200

    fixes in containers and scripts to remove warning and error messages

--- scripts/teardown.sh
+++ scripts/teardown.sh
@@ -1,4 +1,4 @@
-docker-compose -f docker/db-local.yml down
-docker-compose -f docker/db-test.yml down
+docker-compose -f docker/db-local.yml --project-name=nerd-alert down
+docker-compose -f docker/db-test.yml --project-name=nerd-alert-test down
 docker volume prune -f
 ./gradlew cleanJooqSchema
