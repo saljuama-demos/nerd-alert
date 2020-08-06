fe0e55e7a24220e9793656da166ec5ab4843642b
Salvador Juan Martinez <saljuama@gmail.com>
2020-08-06 21:02:15 +0200

    add dummy filters where we will do some JWT business

--- src/main/kotlin/dev/saljuama/demo/nerdalert/Security.kt
+++ src/main/kotlin/dev/saljuama/demo/nerdalert/Security.kt
@@ -1,11 +1,18 @@
 package dev.saljuama.demo.nerdalert
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.core.Authentication
 import org.springframework.security.crypto.factory.PasswordEncoderFactories
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter
+import javax.servlet.FilterChain
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 
 @Configuration
 class Security : WebSecurityConfigurerAdapter() {
@@ -24,6 +31,25 @@ class Security : WebSecurityConfigurerAdapter() {
       .disable()
       .authorizeRequests()
       .antMatchers("/api/**").permitAll()
-      .antMatchers("/**").authenticated()
+      .anyRequest().authenticated()
+      .and()
+      .addFilter(JwtLoginFilter(authenticationManager()))
+      .addFilter(JwtAuthenticationFilter(authenticationManager()))
+  }
+}
+
+class JwtLoginFilter(authenticationManager: AuthenticationManager) : UsernamePasswordAuthenticationFilter() {
+  override fun attemptAuthentication(request: HttpServletRequest, response: HttpServletResponse): Authentication {
+    return super.attemptAuthentication(request, response)
+  }
+
+  override fun successfulAuthentication(request: HttpServletRequest, response: HttpServletResponse, chain: FilterChain, authResult: Authentication) {
+    super.successfulAuthentication(request, response, chain, authResult)
+  }
+}
+
+class JwtAuthenticationFilter(authenticationManager: AuthenticationManager) : BasicAuthenticationFilter(authenticationManager) {
+  override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, chain: FilterChain) {
+    super.doFilterInternal(request, response, chain)
   }
 }
