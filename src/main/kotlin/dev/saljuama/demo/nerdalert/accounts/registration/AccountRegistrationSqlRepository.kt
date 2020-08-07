e4a341bf53dccef3134b4c06d67a98139ba0e882
Salvador Juan Martinez <saljuama@gmail.com>
2020-08-07 16:13:09 +0200

    refactor add static imports to reduce boilerplate

--- src/main/kotlin/dev/saljuama/demo/nerdalert/accounts/registration/AccountRegistrationSqlRepository.kt
+++ src/main/kotlin/dev/saljuama/demo/nerdalert/accounts/registration/AccountRegistrationSqlRepository.kt
@@ -2,8 +2,8 @@ package dev.saljuama.demo.nerdalert.accounts.registration
 
 import arrow.fx.IO
 import dev.saljuama.demo.nerdalert.accounts.AccountNotFoundException
-import dev.saljuama.demos.nerdalert.Tables
-import dev.saljuama.demos.nerdalert.tables.Account
+import dev.saljuama.demos.nerdalert.Tables.ACCOUNT_VERIFICATION
+import dev.saljuama.demos.nerdalert.tables.Account.ACCOUNT
 import org.jooq.DSLContext
 import org.springframework.stereotype.Component
 
@@ -16,11 +16,11 @@ class AccountRegistrationSqlRepository(
     return IO {
       val starterAccount = StarterAccount(account.username, account.email)
       sql
-        .insertInto(Account.ACCOUNT, Account.ACCOUNT.USERNAME, Account.ACCOUNT.EMAIL, Account.ACCOUNT.PASSWORD, Account.ACCOUNT.REGISTERED)
+        .insertInto(ACCOUNT, ACCOUNT.USERNAME, ACCOUNT.EMAIL, ACCOUNT.PASSWORD, ACCOUNT.REGISTERED)
         .values(account.username, account.email, account.password, starterAccount.registered)
         .execute()
       sql
-        .insertInto(Tables.ACCOUNT_VERIFICATION, Tables.ACCOUNT_VERIFICATION.USERNAME, Tables.ACCOUNT_VERIFICATION.TOKEN)
+        .insertInto(ACCOUNT_VERIFICATION, ACCOUNT_VERIFICATION.USERNAME, ACCOUNT_VERIFICATION.TOKEN)
         .values(account.username, starterAccount.verification.token)
         .execute()
       starterAccount
@@ -31,28 +31,28 @@ class AccountRegistrationSqlRepository(
     return IO {
       val result = sql
         .select()
-        .from(Account.ACCOUNT.innerJoin(Tables.ACCOUNT_VERIFICATION).on(Tables.ACCOUNT_VERIFICATION.USERNAME.eq(Account.ACCOUNT.USERNAME)))
-        .where(Account.ACCOUNT.USERNAME.eq(username))
+        .from(ACCOUNT.innerJoin(ACCOUNT_VERIFICATION).on(ACCOUNT_VERIFICATION.USERNAME.eq(ACCOUNT.USERNAME)))
+        .where(ACCOUNT.USERNAME.eq(username))
         .fetchOne()
         ?: throw AccountNotFoundException()
 
       StarterAccount(
-        result.getValue(Account.ACCOUNT.USERNAME),
-        result.getValue(Account.ACCOUNT.EMAIL),
-        result.getValue(Account.ACCOUNT.REGISTERED),
-        AccountVerification(result.getValue(Tables.ACCOUNT_VERIFICATION.TOKEN))
+        result.getValue(ACCOUNT.USERNAME),
+        result.getValue(ACCOUNT.EMAIL),
+        result.getValue(ACCOUNT.REGISTERED),
+        AccountVerification(result.getValue(ACCOUNT_VERIFICATION.TOKEN))
       )
     }
   }
 
   override fun verifyAccount(account: StarterAccount): IO<Unit> {
     return IO {
-      sql.update(Account.ACCOUNT)
-        .set(Account.ACCOUNT.VERIFIED, true)
-        .where(Account.ACCOUNT.USERNAME.eq(account.username))
+      sql.update(ACCOUNT)
+        .set(ACCOUNT.VERIFIED, true)
+        .where(ACCOUNT.USERNAME.eq(account.username))
         .execute()
-      sql.deleteFrom(Tables.ACCOUNT_VERIFICATION)
-        .where(Tables.ACCOUNT_VERIFICATION.USERNAME.eq(account.username))
+      sql.deleteFrom(ACCOUNT_VERIFICATION)
+        .where(ACCOUNT_VERIFICATION.USERNAME.eq(account.username))
         .execute()
       Unit
     }
